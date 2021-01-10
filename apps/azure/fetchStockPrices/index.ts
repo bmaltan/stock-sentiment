7d85d68c17ca0ce8ca6ec277763f92df8b178560
Bugra Murat Altan <bmaltan@live.com>
2021-01-10 17:13:18 +0100

    remove axios dependency, use https instead

--- apps/azure/fetchStockPrices/index.ts
+++ apps/azure/fetchStockPrices/index.ts
@@ -2,22 +2,38 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<any> {
 
-    const axios = require('axios');
+    const https = require('https');
 
-    let stockData = '';
 
-    const ticker = req.query.ticker;
+    const tickers = req.query.tickers;
     const date = new Date(req.query.date);
     const adjustedDate = new Date(date.setDate(date.getDate() - 1)).toISOString().substring(0, 10);
 
-    try {
-        const response = await axios.get(`https://api.unibit.ai/v2/stock/historical?tickers=${ticker}&interval=1&startDate=${adjustedDate}&endDate=${adjustedDate}&selectedFields=all&accessKey=Demo`);
-        stockData = response.data.result_data
-    } catch (error) {
-        console.log(error.response.body);
-    }
+    return new Promise(async function (resolve, reject) {
 
-    return { body: { data: stockData } };
-};
+        let stockDataString = '';
+
+        await https.get(`https://api.unibit.ai/v2/stock/historical?tickers=${tickers}&interval=1&startDate=${adjustedDate}&endDate=${adjustedDate}&selectedFields=all&accessKey=nAo_jAf1LvXfY8W0gVfLRe_QlWsOGvys`, async (resp) => {
+            let data = '';
+
+            await resp.on('data', (chunk) => {
+                stockDataString += chunk;
+            });
+
+            resp.on('end', () => {
+                console.log(JSON.parse(data).explanation);
+            });
+
+            resp.on("error", (err) => {
+                console.log("Error: " + err.message);
+                reject(err);
+            });
+
+            const stockData = JSON.parse(stockDataString);
+
+            resolve({ body: stockData.result_data });
+        });
+    })
+}
 
 export default httpTrigger;
