a85336743bcb43be41a087d0eed76f1a438e9685
Sedat Sert <bmsedat@gmail.com>
2021-01-17 23:28:22 +0100

    reddit connection with snoowrap

--- apps/azure/fetchRedditPosts/reddit-connection.ts
+++ apps/azure/fetchRedditPosts/reddit-connection.ts
@@ -1,26 +1,12 @@
 import * as Snoowrap from 'snoowrap';
-import { FirebaseEnvironmentConfig } from './types/FirebaseEnvironmentConfig';
-
-function getConfig(): FirebaseEnvironmentConfig {
-    return {
-        reddit: {
-            clientId: process.env.REDDIT_CLIENT_ID,
-            clientSecret: process.env.REDDIT_CLIENT_SECRET,
-            username: process.env.REDDIT_USERNAME,
-            password: process.env.REDDIT_PASSWORD,
-        },
-    };
-}
 
 function getSnoowrapClient() {
-    const { reddit: config } = getConfig();
-
     return new Snoowrap({
         userAgent: 'invest-bs-track:::functions:::api',
-        clientId: config.clientId,
-        clientSecret: config.clientSecret,
-        username: config.username,
-        password: config.password,
+        clientId: process.env.REDDIT_CLIENT_ID,
+        clientSecret: process.env.REDDIT_CLIENT_SECRET,
+        username: process.env.REDDIT_USERNAME,
+        password: process.env.REDDIT_PASSWORD,
     });
 }
 
