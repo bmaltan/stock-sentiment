9fad4f4fad80ebec5dd489778955a24fd8f651fc
Bugra Murat Altan <bmaltan@live.com>
2021-02-06 11:47:16 +0100

    comment out favorites
    let's enable this back when we have more content

--- apps/frontend/src/app/shared/dialog-favorites/dialog-favorites.component.ts
+++ apps/frontend/src/app/shared/dialog-favorites/dialog-favorites.component.ts
@@ -1,27 +1,28 @@
-import { Component } from '@angular/core';
-import { MatTableDataSource } from '@angular/material/table';
-import { UserService } from '../services/user.service';
+// import { Component } from '@angular/core';
+// import { MatTableDataSource } from '@angular/material/table';
+// import { PlatformData } from '@invest-track/models';
+// import { PlatformService } from '../services/platform.service';
+// import { UserService } from '../services/user.service';
 
-@Component({
-    selector: 'app-dialog-favorites',
-    templateUrl: './dialog-favorites.component.html',
-    styleUrls: ['./dialog-favorites.component.scss'],
-})
+// @Component({
+//     selector: 'app-dialog-favorites',
+//     templateUrl: './dialog-favorites.component.html',
+//     styleUrls: ['./dialog-favorites.component.scss'],
+// })
 
-export class DialogFavoritesComponent {
+// export class DialogFavoritesComponent {
 
-    favorites: string[] = []
+//     favoritePlatforms?: PlatformData[];
 
-    dataSource: MatTableDataSource<any> = new MatTableDataSource();
-    displayedColumns = ['name', 'actions']
+//     constructor(
+//         private userService: UserService,
+//         private platformService: PlatformService
+//     ) { }
 
-    constructor(
-        private userService: UserService
-    ) { }
+//     ngOnInit() {
+//         const platforms = this.platformService.getPlatforms();
+//         this.userService.getUserFavoritePlatforms().subscribe(favorites => {
 
-    ngOnInit() {
-        this.userService.getUserFavorites().subscribe(favorites => {
-            this.dataSource.data = favorites;
-        });
-    }
-}
+//         });
+//     }
+// }
