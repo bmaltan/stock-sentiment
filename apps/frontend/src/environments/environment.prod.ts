90b3c3079193c42ba39a148e71ab98b169d7b22f
Bugra Murat Altan <bmaltan@live.com>
2021-02-07 12:25:14 +0100

    add prod env

--- apps/frontend/src/environments/environment.prod.ts
+++ apps/frontend/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-    production: true
+    production: false,
+    firebase: {
+        apiKey: 'AIzaSyCUcOsU-JNdXKXTQ6g09NtXQAArwuI3oMs',
+        authDomain: 'bs-invest-track.firebaseapp.com',
+        databaseURL: 'https://bs-invest-track-default-rtdb.europe-west1.firebasedatabase.app',
+        projectId: 'bs-invest-track',
+        storageBucket: 'bs-invest-track.appspot.com',
+        messagingSenderId: '397473028742',
+        appId: '1:397473028742:web:7e3e87c88133a0d38a8f07',
+        measurementId: 'G-1WJ40DB4ZH'
+    }
 };
