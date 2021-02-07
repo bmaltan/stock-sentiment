ec5d36886d2d29c723ff571740ab9e6097914845
Bugra Murat Altan <bmaltan@live.com>
2021-02-07 08:25:46 +0100

    handle single and no decimals (1 as 1.00 and 1.1 as 1.10)

--- apps/frontend/src/app/shared/pipes/shorten-decimal.pipe.ts
+++ apps/frontend/src/app/shared/pipes/shorten-decimal.pipe.ts
@@ -4,10 +4,16 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class ShortenDecimalPipe implements PipeTransform {
     transform(value: string | number): string | number {
         if (typeof value === 'number') {
+            console.log(value)
+            if (value % 1 === 0) {
+                return value.toString() + '.00';
+            } else if (value.toString().split('.')[1].length === 1) {
+                return value.toString() + '0';
+            }
             return value.toString().length > 6 ? Math.round(value) : value;
         } else {
             if (value?.indexOf('.') === 0) {
-                return '0' + value.substring(0, 4);
+                return '0' + value.substring(0, 5);
             } else {
                 return value?.includes('.') ? value.split('.')[0] : value;
             }
