096713e3b98c737bb0cd16c619268d2b63da6443
Bugra Murat Altan <bmaltan@live.com>
2021-02-18 17:37:27 +0100

    fix the decimal issue with yahoo data

--- apps/frontend/src/app/shared/pipes/shorten-decimal.pipe.ts
+++ apps/frontend/src/app/shared/pipes/shorten-decimal.pipe.ts
@@ -2,19 +2,19 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({ name: 'shortenDecimal' })
 export class ShortenDecimalPipe implements PipeTransform {
-    transform(value: string | number): string | number {
+    transform(value: number | string): string {
+
         if (typeof value === 'number') {
-            if (value % 1 === 0) {
-                return value.toString() + '.00';
-            } else if (value.toString().split('.')[1].length === 1) {
-                return value.toString() + '0';
-            }
-            return value.toString().length > 6 ? Math.round(value) : value;
+            return roundToTwoDecimals(value);
         } else {
-            if (value?.indexOf('.') === 0) {
-                return '0' + value.substring(0, 5);
+            return roundToTwoDecimals(parseFloat(value));
+        }
+
+        function roundToTwoDecimals(value: number) {
+            if (value > 0.01) {
+                return value.toFixed(2);
             } else {
-                return value?.includes('.') ? value.split('.')[0] : value;
+                return value.toFixed(4);
             }
         }
     }
