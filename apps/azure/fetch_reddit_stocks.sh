26a9ef0a5236b1820b98384be1cb1ec1a1ba3d4e
Sedat Sert <bmsedat@gmail.com>
2021-02-21 00:59:27 +0100

    get yesterday's date for the bash script to be able to automate better

--- apps/azure/fetch_reddit_stocks.sh
+++ apps/azure/fetch_reddit_stocks.sh
@@ -1,8 +1,16 @@
-pipenv run python reddit.py 2021-02-19 investing &
-pipenv run python reddit.py 2021-02-19 pennystocks &
-pipenv run python reddit.py 2021-02-19 stocks &
-pipenv run python reddit.py 2021-02-19 stockmarket &
-pipenv run python reddit.py 2021-02-19 stock_picks &
-pipenv run python reddit.py 2021-02-19 wallstreetbets &
-pipenv run python reddit.py 2021-02-19 daytrading &
-pipenv run python reddit.py 2021-02-19 robinhoodpennystocks
+fetch_reddit_stocks() {
+    yesterdate="$(date -d "yesterday 13:00" '+%Y-%m-%d')"
+
+    echo "$yesterdate"
+
+    pipenv run python reddit.py $yesterdate investing &
+    pipenv run python reddit.py $yesterdate pennystocks &
+    pipenv run python reddit.py $yesterdate stocks &
+    pipenv run python reddit.py $yesterdate stockmarket &
+    pipenv run python reddit.py $yesterdate stock_picks &
+    pipenv run python reddit.py $yesterdate wallstreetbets &
+    pipenv run python reddit.py $yesterdate daytrading &
+    pipenv run python reddit.py $yesterdate robinhoodpennystocks
+}
+
+fetch_reddit_stocks
