5355d6127c859b997f3365a4643928af06349a18
Sedat Sert <bmsedat@gmail.com>
2021-02-28 21:11:30 +0100

    move script files to another folder

---
+++ tools/data-script/fetch_reddit_stocks.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+yesterdate="$(date -d "yesterday 13:00" '+%Y-%m-%d')"
+
+echo "$yesterdate"
+
+pipenv run python reddit.py $yesterdate investing &
+pipenv run python reddit.py $yesterdate pennystocks &
+pipenv run python reddit.py $yesterdate stocks &
+pipenv run python reddit.py $yesterdate stockmarket &
+pipenv run python reddit.py $yesterdate stock_picks &
+pipenv run python reddit.py $yesterdate wallstreetbets &
+pipenv run python reddit.py $yesterdate daytrading &
+pipenv run python reddit.py $yesterdate robinhoodpennystocks
