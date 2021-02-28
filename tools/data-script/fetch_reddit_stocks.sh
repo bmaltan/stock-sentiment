db85c4d1358369b10e37888daa2a7a9df1832044
Sedat Sert <bmsedat@gmail.com>
2021-02-28 22:21:11 +0100

    add crypto subreddits to sh script

--- tools/data-script/fetch_reddit_stocks.sh
+++ tools/data-script/fetch_reddit_stocks.sh
@@ -11,4 +11,9 @@ pipenv run python reddit.py $yesterdate stockmarket &
 pipenv run python reddit.py $yesterdate stock_picks &
 pipenv run python reddit.py $yesterdate wallstreetbets &
 pipenv run python reddit.py $yesterdate daytrading &
-pipenv run python reddit.py $yesterdate robinhoodpennystocks
+pipenv run python reddit.py $yesterdate robinhoodpennystocks & 
+
+pipenv run python reddit.py 2021-02-27 cryptocurrency &
+pipenv run python reddit.py $yesterdate cryptomarkets &
+pipenv run python reddit.py $yesterdate crypto_currency_news &
+pipenv run python reddit.py $yesterdate cryptocurrencies
