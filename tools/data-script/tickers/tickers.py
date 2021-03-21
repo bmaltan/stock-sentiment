8ba979567da8f4abafc725b76e109f2ff7897fac
Sedat Sert <sedatsert@posteo.me>
2021-03-21 22:06:57 +0100

    get tickers from stock and crypto json files

--- tools/data-script/tickers/tickers.py
+++ tools/data-script/tickers/tickers.py
@@ -1,28 +1,31 @@
 import json
-from enum import Enum
-from collections import namedtuple
 from typing import List
+from models.Ticker import InvestTicker
+from models.TickerType import TickerType
 
 
-class TickerType(Enum):
-    Stock = 1
-    Crypto = 2
-
-
-Ticker = namedtuple('Ticker', ['symbol', 'type'])
-
-
-def get_tickers() -> List[Ticker]:
-    result = []
-
+def get_stocks() -> List[InvestTicker]:
     with open('./tickers.json', encoding='utf-8') as tickers_json:
         stock_tickers = json.loads(tickers_json.read())
-        result += [Ticker(x["symbol"], TickerType.Stock)
-                   for x in stock_tickers]
+        stock_tickers = [InvestTicker(x["symbol"], TickerType.Stock)
+                         for x in stock_tickers]
+        return stock_tickers
 
+
+def get_crypto() -> List[InvestTicker]:
     with open('./cryptos.json', encoding='utf-8') as cryptos_json:
         crypto_tickers = json.loads(cryptos_json.read())
-        result += [Ticker(x["symbol"], TickerType.Crypto)
-                   for x in crypto_tickers]
+        crypto_tickers = [InvestTicker(x["symbol"], TickerType.Crypto)
+                          for x in crypto_tickers]
+        return crypto_tickers
 
-    return result
+
+def get_tickers(type: TickerType = None) -> List[InvestTicker]:
+    if type == TickerType.All or type is None:
+        return get_crypto() + get_stocks()
+    elif type == TickerType.Crypto:
+        return get_crypto()
+    elif type == TickerType.Stock:
+        return get_stocks()
+    else:
+        return []
