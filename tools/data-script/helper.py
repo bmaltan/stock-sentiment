691cd362b09fdeeab1dc408fed60d9470bbd7c20
Sedat Sert <sedatsert@posteo.me>
2021-04-25 18:17:30 +0200

    fix(data): count ticker only once per post

--- tools/data-script/helper.py
+++ tools/data-script/helper.py
@@ -3,17 +3,18 @@ from typing import List
 from models.Ticker import InvestTicker
 
 
-def split_to_words(s: str) -> List[str]:
+def split_to_unique_words(s: str) -> List[str]:
     s = re.sub(r'[\.,\/#!\$\%\^&\*;:\+-\?{}\[\]"\'=\-_`<>~\(\)]', ' ', s)
     s = re.sub(r'\s{2,}', ' ', s)
-    return s.split()
+    s = set(s.split())
+    return list(s)
 
 
 def find_mentioned_tickers(s: str, tickers: List[InvestTicker]) -> List[InvestTicker]:
     result = []
 
     # count ticker however many is mentioned
-    for word in split_to_words(s):
+    for word in split_to_unique_words(s):
         if word in tickers:
             index = tickers.index(word)
             result.append(tickers[index])
